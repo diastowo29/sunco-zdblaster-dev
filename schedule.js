c79a43e550cf469dcafe06efd24d8f7646ba2b1a
Diastowo <diastowo@gmail.com>
2023-08-01 17:50:57 +0700

    add startSchedule

--- schedule.js
+++ schedule.js
@@ -2,5 +2,10 @@ let Queue = require('bull');
 let REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
 
 let workQueue = new Queue('work', REDIS_URL);
-let job = await workQueue.add();
-console.log(job)
+
+startSchedule()
+
+async function startSchedule () {
+    let job = await workQueue.add();
+    console.log(job)
+}
