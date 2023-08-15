5c69cc8c592848c5603ed943ca9b51019b9890b8
Diastowo <diastowo@gmail.com>
2023-08-15 16:25:29 +0700

    test

--- config/redis.config.js
+++ config/redis.config.js
@@ -2,7 +2,11 @@ let Queue = require('bull');
 let REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
 var redis_url = require('url').parse(REDIS_URL)
 
-let workQueue = new Queue('job6', REDIS_URL);
+let workQueue = new Queue('job7', REDIS_URL, {
+    settings: {
+        maxStalledCount: 0
+    }
+});
 
 
 module.exports=workQueue
