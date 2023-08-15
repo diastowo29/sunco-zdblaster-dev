186b709e6f9df84e4b91b7574046e9f30b5f06cc
Diastowo <diastowo@gmail.com>
2023-08-15 16:22:22 +0700

    test git pu

--- config/redis.config.js
+++ config/redis.config.js
@@ -2,11 +2,7 @@ let Queue = require('bull');
 let REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
 var redis_url = require('url').parse(REDIS_URL)
 
-let workQueue = new Queue('job6', {redis: {
-    port: redis_url.port,
-    host: redis_url.hostname,
-    db: redis_url.db
-}});
+let workQueue = new Queue('job6', REDIS_URL);
 
 
 module.exports=workQueue
