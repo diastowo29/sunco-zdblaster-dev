04b7bf3ba2ec4ed169826fe7c227759e47c7727c
Diastowo <diastowo@gmail.com>
2023-08-15 15:37:46 +0700

    update schedule

--- schedule.js
+++ schedule.js
@@ -1,8 +1,6 @@
 let Queue = require('bull');
 let REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
-let workQueue = new Queue('blastWaJob', {
-    redis: REDIS_URL
-  });
+let workQueue = new Queue('blastWaJob', REDIS_URL);
 const db = require("./models");
 const Job = db.job;
 
